0f8aef4534b7c4325ef6ecd696faee11639847e1
ceb10n <rafaelomarques@gmail.com>
2012-08-06 16:51:35 -0300

    euler problem #1

---
+++ 001.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    total := 0
+    for i := 1; i < 1000; i++ {
+        if i % 3 == 0 || i % 5 == 0 {
+            total += i;
+        }        
+    }
+    fmt.Printf("Sum %d", total)
+}
